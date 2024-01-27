c2aa890ad611dd9c2bed47cf6af13b8737ec421a
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-28 03:19:19 +0700

    feat: add `search-input` feature components

--- src/lib/components/features/search-input.svelte
+++ src/lib/components/features/search-input.svelte
@@ -1,3 +1,16 @@
-<div>
-  <input type="text" aria-label="Search" />
-</div>
+<script>
+  export let placeholder = 'Search...';
+  export let value = '';
+
+  function handleChange(event) {
+    value = event.target.value;
+  }
+</script>
+
+<input
+  type="text"
+  {placeholder}
+  class="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none"
+  bind:value
+  on:input={handleChange}
+/>
