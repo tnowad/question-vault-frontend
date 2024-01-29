41063b4563a3da1873b64512f2edb52acfff394c
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-29 12:34:37 +0700

    feat: responsive left-sidebar

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -8,12 +8,12 @@
 
 <main>
   <div class="container mx-auto grid grid-cols-12">
-    <div class="col-span-3">
+    <div class="hidden md:block md:col-span-4 lg:col-span-3 xl:col-span-2">
       <div class="top-16 sticky h-auto">
         <LeftSidebar />
       </div>
     </div>
-    <div class="col-span-9">
+    <div class="col-auto">
       <slot />
     </div>
   </div>
