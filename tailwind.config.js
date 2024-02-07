1ad5bbe4d22ec5118819db2f7c4f2690e39ebbd3
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-08 02:29:44 +0700

    chore: add flowbite-svelte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+  ],
   theme: {
     extend: {
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-        },
-      },
+      // container: {
+      //   center: true,
+      //   padding: {
+      //     DEFAULT: '1rem',
+      //     sm: '2rem',
+      //     lg: '4rem',
+      //     xl: '5rem',
+      //   },
+      // },
       colors: {
         primary: {
-          50: '#FFF5F2',
-          100: '#FFF1EE',
-          200: '#FFE4DE',
-          300: '#FFD5CC',
-          400: '#FFBCAD',
-          500: '#FE795D',
-          600: '#EF562F',
-          700: '#EB4F27',
-          800: '#CC4522',
-          900: '#A5371B',
+          50: '#eff6ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          300: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+          800: '#1e40af',
+          900: '#1e3a8a',
         },
       },
     },
   },
   darkMode: ['class'],
-  plugins: [],
+  plugins: [
+    // require('flowbite/plugin')
+  ],
 };
