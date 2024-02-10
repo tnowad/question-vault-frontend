8ef510798e219e7f9cd0da8629dba436a3cfe2be
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-11 06:19:02 +0700

    chore: remove search-input and theme-switcher

--- src/lib/components/features/theme-switcher/index.ts
+++
@@ -1,6 +0,0 @@
-import ThemeSwitcher from './theme-switcher.svelte';
-import { updateTheme, initializeTheme } from './dark-mode';
-
-export default ThemeSwitcher;
-
-export { initializeTheme, updateTheme };
