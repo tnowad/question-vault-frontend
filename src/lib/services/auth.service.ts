bddb5cabd5290c2a3d1e5f5e79fb4419865bb479
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-12 13:55:52 +0700

    feat: add type for sign-in response

--- src/lib/services/auth.service.ts
+++ src/lib/services/auth.service.ts
@@ -1,34 +1,54 @@
+import type { StatusCodes } from 'http-status-codes';
+
 type SignInBody = {
   email: string;
   password: string;
 };
 
 type SignInSuccessful = {
-  accessToken: string;
-  refreshToken: string;
-};
-
-type SignInFailed = {
+  statusCode: StatusCodes.ACCEPTED;
+  data: {
+    user: {
+      id: number;
+      uuid: string;
+      fullname: string;
+      phone: string;
+      username: string;
+      birthdate: string;
+      createdAt: string;
+      updatedAt: string;
+      deletedAt: string;
+    };
+    token: {
+      accessToken: string;
+      refreshToken: string;
+    };
+  };
   message: string;
 };
 
-const signIn = async (params: SignInBody): Promise<SignInSuccessful | SignInFailed | Error> => {
+type SignInFailed = {
+  statusCode: StatusCodes.BAD_REQUEST;
+  message: string | string[];
+  error: string;
+};
+
+type SignInResponse = Omit<Response, 'json'> & {
+  json: () => Promise<SignInSuccessful | SignInFailed>;
+};
+
+const signIn = async (params: SignInBody) => {
   try {
-    const url = new URL('/auth/email/sign-in');
-    const response = await fetch(url, {
+    const url = new URL('/auth/email/sign-in', 'http://localhost:3000');
+    const response = (await fetch(url, {
       method: 'POST',
       body: JSON.stringify(params),
       headers: {
         'Content-Type': 'application/json',
       },
-    });
+    })) as SignInResponse;
 
-    if (response.status === 200) {
-      return response.json() as Promise<SignInSuccessful>;
-    } else if (response.status === 400) {
-      return response.json() as Promise<SignInFailed>;
-    }
-    return Error('Unexpected response from server');
+    return response.json();
   } catch (error) {
     throw new Error('Failed to sign in: ' + (error as Error).message);
   }
